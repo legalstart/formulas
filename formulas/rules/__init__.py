55f0cc3c6ddd4815142188dd3b039b7fef4911f1
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2016-04-18 22:19:01 +0200

    Remove . considerations in varnames and add initial_vars stuff + fix tests

--- formulas/rules/__init__.py
+++ formulas/rules/__init__.py
@@ -8,9 +8,12 @@ from formulas.rules_list import RulesList
 
 _RULES_DIR = os.path.dirname(os.path.realpath(__file__))
 
+_INITIAL_VARS_DIR = os.path.join(os.path.dirname(_RULES_DIR), 'initial_vars')
 
-def extract_rules(filepath):
-    yaml.add_constructor(u'tag:yaml.org,2002:omap:', construct_odict)
+yaml.add_constructor(u'tag:yaml.org,2002:omap:', construct_odict)
+
+
+def extract_yaml_obj(filepath):
     with open(filepath, 'r') as f:
         return yaml.load(f)
 
@@ -18,7 +21,8 @@ def extract_rules(filepath):
 RULESETS = [
     RulesList(
         filename[:len(filename) - 4],  # "xxx.yml"
-        extract_rules(os.path.join(_RULES_DIR, filename)).items()
+        extract_yaml_obj(os.path.join(_RULES_DIR, filename)).items(),
+        extract_yaml_obj(os.path.join(_INITIAL_VARS_DIR, filename))
     )
 
     for filename in os.listdir(_RULES_DIR)
