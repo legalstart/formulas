0d971f3be0bd19555eb869012cb146b05a505515
Vincent Perez <vperez@yolaw.fr>
2016-04-18 18:54:10 +0200

    renaming variables in decorator

--- meta_test/decorators.py
+++ meta_test/decorators.py
@@ -21,19 +21,19 @@ def retrieve_test_methods(klass):
     return [m[1] for m in methods if hasattr(m[1], '__to_test__')]
 
 
-def class_dec(testable_list):
+def class_dec(testable_lists):
     def class_decorator(klass):
         test_methods = retrieve_test_methods(klass)
-        for testable_obj in testable_list:
+        for testable_list in testable_lists:
             attrs = {}
             for m in test_methods:
-                for i, rule in enumerate(testable_obj):
-                    decorated_m = currying(rule)(m)
+                for i, testable_obj in enumerate(testable_list):
+                    decorated_m = currying(testable_obj)(m)
                     decorated_m.__test__ = 1
                     test_name = "Test_%s_on_testable_obj_%d" % (m.__name__, i + 1)
                     attrs[test_name] = decorated_m
 
-            tl_test_klass = type("test_%s" % testable_obj.name, (klass,), attrs)
+            tl_test_klass = type("test_%s" % testable_list.name, (klass,), attrs)
             setattr(modules[klass.__module__], tl_test_klass.__name__,
                     tl_test_klass)
     return class_decorator
