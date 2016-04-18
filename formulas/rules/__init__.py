461c5e02a3a1ea11e027ed3080f2ba5c57e62555
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2016-04-18 17:38:31 +0200

    Ordered Yaml + formulas.rules.RULESETS as list of RuleSet

--- formulas/rules/__init__.py
+++ formulas/rules/__init__.py
@@ -1,18 +1,69 @@
 import os
 import yaml
+from collections import OrderedDict
+
+from test_lib import RuleSet
 
 
 _RULES_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
+# From https://gist.github.com/weaver/317164
+def construct_odict(load, node):
+    """This is the same as SafeConstructor.construct_yaml_omap(),
+    except the data type is changed to OrderedDict() and setitem is
+    used instead of append in the loop.
+    >>> yaml.load('''
+    ... !!omap
+    ... - foo: bar
+    ... - mumble: quux
+    ... - baz: gorp
+    ... ''')
+    OrderedDict([('foo', 'bar'), ('mumble', 'quux'), ('baz', 'gorp')])
+    >>> yaml.load('''!!omap [ foo: bar, mumble: quux, baz : gorp ]''')
+    OrderedDict([('foo', 'bar'), ('mumble', 'quux'), ('baz', 'gorp')])
+    """
+
+    omap = OrderedDict()
+    yield omap
+    if not isinstance(node, yaml.SequenceNode):
+        raise yaml.constructor.ConstructorError(
+            "while constructing an ordered map",
+            node.start_mark,
+            "expected a sequence, but found %s" % node.id, node.start_mark
+        )
+    for subnode in node.value:
+        if not isinstance(subnode, yaml.MappingNode):
+            raise yaml.constructor.ConstructorError(
+                "while constructing an ordered map", node.start_mark,
+                "expected a mapping of length 1, but found %s" % subnode.id,
+                subnode.start_mark
+            )
+        if len(subnode.value) != 1:
+            raise yaml.constructor.ConstructorError(
+                "while constructing an ordered map", node.start_mark,
+                "expected a single mapping item, but found %d items"
+                % len(subnode.value),
+                subnode.start_mark
+            )
+        key_node, value_node = subnode.value[0]
+        key = load.construct_object(key_node)
+        value = load.construct_object(value_node)
+        omap[key] = value
+
+
 def extract_rules(filepath):
+    yaml.add_constructor(u'tag:yaml.org,2002:omap:', construct_odict)
     with open(filepath, 'r') as f:
         return yaml.load(f)
 
 
-RULESETS = {
-    filename[:len(filename) - 4]:  # "xxx.yml"
-        extract_rules(os.path.join(_RULES_DIR, filename))
+RULESETS = [
+    RuleSet(
+        name=filename[:len(filename) - 4],  # "xxx.yml"
+        rules=extract_rules(os.path.join(_RULES_DIR, filename))
+    )
+
     for filename in os.listdir(_RULES_DIR)
     if filename.endswith('.yml')
-}
+]
--- test_lib/ruleset.py
+++ test_lib/ruleset.py
@@ -1,6 +1,10 @@
 class RuleSet(object):
 
+    __slots__ = (
+        'name',  # str
+        'rules'  # ordered iterable
+    )
+
     def __init__(self, name, rules):
         self.name = name
         self.rules = rules
-
